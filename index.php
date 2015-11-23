30e314f833b270d5fa93a0bf84b8714ed866c99f
Augusto Bonilla <Augusto.bonilla@gmail.com>
2015-11-23 11:12:27 -0500

    creando proyecto

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<h1>bienvenidos a heroku</h1>
+<form action="" method="POST" role="form">
+	<legend>Form title</legend>
+
+	<div class="form-group">
+		<label for="">label</label>
+		<input type="text" class="form-control" id="" placeholder="Input field">
+	</div>
+
+	
+
+	<button type="submit" class="btn btn-primary">Submit</button>
+</form>
+	
+</body>
+</html>
